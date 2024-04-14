f1d2af2f70ac3c762d67ea64dab29b4c7faa9f1b
AhmadJanuar1 <ahmadjanuaramri2015@gmail.com>
2024-04-14 13:48:13 +0700

    add file projectData

---
+++ src/projectData.js
@@ -0,0 +1,36 @@
+const PROJECT_LIST = [
+  {
+    id: 1,
+    image: "../../public/img/portofolio.png",
+    title: "Personal Portofolio Website",
+    description: "Web Design, Website Development",
+    year: "(2023)",
+    link: "https://github.com/AhmadJanuar1/personal-portofolio-website-1.0",
+  },
+  {
+    id: 2,
+    image: "../../public/img/sistem-pakar.png",
+    title:
+      "Penerapan Metode Forward Chaining Pada Sistem Pakar Diagnosa Penyakit Ayam Ternak",
+    description: "Php, JavaScript, HTML, CSS",
+    year: "(2023)",
+    link: "https://github.com/AhmadJanuar1/Penerapan-Metode-Forward-Chaining-Pada-Sistem-Pakar-Diagnosa-Penyakit-Ayam-Ternak",
+  },
+  {
+    id: 3,
+    image: "../../public/img/pariwsiata.png",
+    title: "Pariwisata - Landing Page",
+    description: "Pengembangan Website Pariwisata ",
+    year: "(2023)",
+    link: "https://github.com/AhmadJanuar1/web-app-1",
+  },
+  {
+    id: 4,
+    image: "../../public/img/weather.png",
+    title: "Weather App - Api Openweather",
+    description: "Web Design, Website Development",
+    year: "(2024)",
+    link: "https://github.com/AhmadJanuar1/01-practice-weather-api",
+  },
+];
+export default PROJECT_LIST;
