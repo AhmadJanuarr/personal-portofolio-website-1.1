e706fef2afb57752362131c13a75397041d1be7f
AhmadJanuar1 <ahmadjanuaramri2015@gmail.com>
2024-05-14 07:44:43 +0700

    clean code

--- src/component/Template.jsx
+++ src/component/Template.jsx
@@ -1,6 +1,6 @@
 export function Template({ children, darkMode }) {
   return (
-    <div className={`  ${darkMode ? "dark" : ""} `}>
+    <div className={`${darkMode ? "dark" : ""}`}>
       <div className="w-full h-full bg-[#fffff] over text-black dark:bg-black dark:text-white px-4 laptop:px-5 tablet:px-10">
         {children}
       </div>
